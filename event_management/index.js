712e7564479352b85a4934eaf612d3c8d2709186
archanaCoddle <archana.sk@coddletech.com>
2023-10-13 17:09:39 +0530

    event

---
+++ event_management/index.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// const express = require('express');
+
+// const server = express();
+// const users = [];
+// server.post('/', (req, res) => {
+//   const newUser = req.query;
+//   users.push(newUser);
+//   res.send('Signup successful');
+//   console.log(users);
+// });
+
+// server.listen(3000, () => {
+//   console.log('Server is running on http://localhost:3000');
+// });
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+server.use(bodyParser.json());
+
+const users = [];
+
+server.post('/', (req, res) => {
+  const {
+    firstname, lastname, username, password,
+  } = req.body;
+  const newUser = {
+    firstname, lastname, username, password,
+  };
+  console.log(newUser);
+  users.push(newUser);
+  res.send('Signup successful');
+  console.log(users);
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
