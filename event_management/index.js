5b9cf3c306ce72b8c005f73c5e44952846fd8f71
archanaCoddle <archana.sk@coddletech.com>
2023-10-17 11:13:29 +0530

    event

--- event_management/index.js
+++ event_management/index.js
@@ -1,20 +1,58 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql2');
 const sample = require('./sample');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Ammu123@',
+  database: 'event_management',
+});
+con.connect((err) => {
+  if (err) {
+    console.log(`database error${err.message}`);
+  } else {
+    console.log('connected');
+  }
+});
+
+const secretKey = 'Archana';
 
 const server = express();
 server.use(bodyParser.json());
 const users = [];
 
+function verifying(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader === undefined) {
+    res.send('no token provided');
+  }
+  const token = authHeader.split(' ')[1];
+  // eslint-disable-next-line no-unused-vars
+  jwt.verify(token, secretKey, (err) => {
+    if (err) {
+      res.send('authentication failed', err.message);
+    } else {
+      next();
+    }
+  });
+}
+
+server.post('/', verifying, (_req, res) => {
+  res.send('Welcome');
+});
 server.post('/signup', (req, res) => {
   sample.sample();
   const {
-    firstname, lastname, username, password,
+    id, firstname, lastname, phone, email, address, username, password,
   } = req.body;
   const newUser = {
-    firstname, lastname, username, password,
+    id, firstname, lastname, phone, email, address, username, password,
   };
+  con.query(`INSERT INTO customer (id, first_name, last_name, phone, email, address, username, password) VALUES ('${id}', '${firstname}', '${lastname}', '${phone}', '${email}', '${address}', '${username}', '${password}')`);
   console.log(newUser);
   users.push(newUser);
   res.send('Signup successful');
@@ -22,16 +60,31 @@ server.post('/signup', (req, res) => {
 });
 
 server.post('/login', (req, res) => {
-  const {
-    username, password,
-  } = req.body;
-  const user = users.find((u) => u.username === username && u.password === password);
-  if (user) {
-    res.send('login succefull');
-  } else {
-    res.send('invalid username/password');
-  }
-  console.log(users);
+  const { username, password } = req.body;
+  con.query('SELECT username, password FROM customer', (loginErr, loginResults) => {
+    if (loginErr) {
+      console.log('Database error:', loginErr);
+    } else {
+      const user = loginResults.find((u) => u.username === username && u.password === password);
+      if (user) {
+        jwt.sign({ user }, secretKey, { expiresIn: '86400s' }, (_jwtErr, token) => {
+          res.send({ auth: true, token });
+        });
+      } else {
+        res.send('Invalid username/password');
+      }
+    }
+  });
+});
+
+server.get('/events', verifying, (req, res) => {
+  con.query('SELECT * FROM package_categories', (err, querys) => {
+    if (err) {
+      console.log('Database error:', err);
+    } else {
+      res.send({ querys });
+    }
+  });
 });
 
 server.listen(3000, () => {
--- event_management/sample.js
+++ event_management/sample.js
@@ -1,4 +1,7 @@
-module.exports.sample = function () {
+console.log(__filename, '    sample path');
+console.log(__dirname, '  main directory');
+
+exports.sample = function () {
   global.console.log('bllaablaaaa');
 };
 
