baf4c6c42439ca926760905f3803907301a492cd
archanaCoddle <archana.sk@coddletech.com>
2023-10-18 09:58:09 +0530

    event

--- event_management/controllers/eventControllers.js
+++ event_management/controllers/eventControllers.js
@@ -1,23 +1,45 @@
+const jwt = require('jsonwebtoken');
 const eventModel = require('../models/event');
 
-function allEvent(req, res) {
-  eventModel.allEvent(req, (loginErr, user) => {
-    if (loginErr) {
-      console.log('Database error:', loginErr);
+const secretKey = 'Archana';
+function verifying(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (authHeader === undefined) {
+    res.send('no token provided');
+  }
+  const token = authHeader.split(' ')[1];
+  // eslint-disable-next-line no-unused-vars
+  jwt.verify(token, secretKey, (err) => {
+    if (err) {
+      res.send('authentication failed', err.message);
     } else {
-      console.log('controller', user);
-      res.send({ user });
+      next();
     }
   });
 }
+
+function allEvent(req, res) {
+  verifying(req, res, () => {
+    eventModel.allEvent(req, (loginErr, user) => {
+      if (loginErr) {
+        console.log('Database error:', loginErr);
+      } else {
+        console.log('controller', user);
+        res.send({ user });
+      }
+    });
+  });
+}
 function eventSelected(req, res) {
-  const eventClicked = req.body.eventSelected;
-  eventModel.eventSelected(eventClicked, (err, data) => {
-    if (err) {
-      console.log('Database error:', err);
-    } else {
-      res.send({ data });
-    }
+  verifying(req, res, () => {
+    const eventClicked = req.body.eventSelected;
+    eventModel.eventSelected(eventClicked, (err, data) => {
+      if (err) {
+        console.log('Database error:', err);
+      } else {
+        res.send({ data });
+      }
+    });
   });
 }
 
