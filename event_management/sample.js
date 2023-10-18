b1de5d431ab0f284084b41f77943353b3bfa15df
archanaCoddle <archana.sk@coddletech.com>
2023-10-18 09:31:50 +0530

    event

--- event_management/sample.js
+++
@@ -1,8 +0,0 @@
-console.log(__filename, '    sample path');
-console.log(__dirname, '  main directory');
-
-exports.sample = function () {
-  global.console.log('bllaablaaaa');
-};
-
-// module.export.sample = sample;
